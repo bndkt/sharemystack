49f8ed9b5ae934cd778c47c7662f04ac43cf08c5
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-07 12:52:48 +0800

    Add Babel plugins

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [require.resolve("expo-router/babel")],
+    plugins: [
+      require.resolve("expo-router/babel"),
+      // NOTE: this is required to pass the right environment
+      [
+        "transform-inline-environment-variables",
+        {
+          include: "TAMAGUI_TARGET",
+        },
+      ],
+      // NOTE: this is optional, you don't *need* the compiler
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      // NOTE: this is only necessary if you are using reanimated for animations
+      "react-native-reanimated/plugin",
+    ],
   };
 };
