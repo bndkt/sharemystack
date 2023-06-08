995edf0151222ca8962a50ab4891ca0875d8d690
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-08 09:05:20 +0800

    Add mmkv and types

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,29 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
+import { MMKV } from "react-native-mmkv";
+
+export const mmkv = new MMKV();
+
+const storage = {
+  setItem: (key: string, data: string) => mmkv.set(key, data),
+  getItem: (key: string) => mmkv.getString(key),
+  removeItem: (key: string) => mmkv.delete(key),
+};
+
+// TMP until Expo SDK 49
+process.env.SUPABASE_URL = "http://localhost:54321"; // "https://wmruscfwlagunazmjwhh.supabase.co"
+process.env.SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"; // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtcnVzY2Z3bGFndW5hem1qd2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYxMDU1NjgsImV4cCI6MjAwMTY4MTU2OH0.4pFZiBa1ZtkYKTtscdRM4dnBtX55iqMBur_wNZygEqQ"
+
+import { Database } from "./database.types";
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  "http://localhost:54321", // "https://wmruscfwlagunazmjwhh.supabase.co", // process.env.SUPABASE_URL,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0" // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtcnVzY2Z3bGFndW5hem1qd2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYxMDU1NjgsImV4cCI6MjAwMTY4MTU2OH0.4pFZiBa1ZtkYKTtscdRM4dnBtX55iqMBur_wNZygEqQ" // process.env.SUPABASE_KEY
+export const supabase = createClient<Database>(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      storage,
+    },
+  }
 );
