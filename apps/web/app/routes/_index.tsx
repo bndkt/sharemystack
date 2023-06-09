575cd71a6c5cbabf60529562dfdbcec92830feaa
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-09 22:33:52 +0800

    Add color

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,3 +1,5 @@
 export default function Index() {
-  return <div className="text-center pt-36">Share My Stack</div>;
+  return (
+    <div className="text-center pt-36 text-sharemystack">Share My Stack</div>
+  );
 }
