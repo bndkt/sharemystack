180e05de493b233d4b4a298d61dbdaf69b0b7eab
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-09 23:04:24 +0800

    Configure Sentry

--- apps/app/app/_layout.tsx
+++ apps/app/app/_layout.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TamaguiProvider, Theme } from "tamagui";
 
 import "../lib/polyfill";
+import "../lib/sentry";
 import { AuthProvider } from "../components/providers/AuthProvider";
 import { NavigationThemeProvider } from "../components/providers/NavigationThemeProvider";
 import config from "../tamagui.config";
---
+++ apps/app/lib/sentry.ts
@@ -0,0 +1,7 @@
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://b8b90433e45342b08a64f3288673632b@o4504655409577984.ingest.sentry.io/4505330170200064",
+  enableInExpoDevelopment: true,
+  debug: true,
+});
