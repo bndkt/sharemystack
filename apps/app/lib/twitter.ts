94b142a8314911a89f1a7ae18f27e239694cf1bd
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-15 14:09:01 +0800

    Add comment

--- apps/app/lib/twitter.ts
+++ apps/app/lib/twitter.ts
@@ -2,4 +2,4 @@ import { Client } from "twitter-api-sdk";
 
 export const twitter = new Client(
   "AAAAAAAAAAAAAAAAAAAAALEsoAEAAAAA3A7r9C7QXH2XLgU0o9cp1eMZBIU%3DZzTXSNYKATvzBSWJsMZr27LiDt3GwZ4KJmQAOt1sxyIavHW7Th"
-);
+); // TODO
