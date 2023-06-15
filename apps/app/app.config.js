0cec31223c989681acd50e1e2a60e58cb3833893
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-15 11:01:53 +0800

    Change to JS

--- apps/app/app.config.js
+++ apps/app/app.config.js
@@ -1,63 +1,63 @@
-{
-  "expo": {
-    "name": "Share My Stack",
-    "slug": "sharemystack",
-    "scheme": "sharemystack",
-    "owner": "mueller",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+module.exports = {
+  expo: {
+    name: "Share My Stack",
+    slug: "sharemystack",
+    scheme: "sharemystack",
+    owner: "mueller",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0,
-      "url": "https://u.expo.dev/987c3c40-d46a-45e7-a545-112f3c6e23e9"
+    updates: {
+      fallbackToCacheTimeout: 0,
+      url: "https://u.expo.dev/987c3c40-d46a-45e7-a545-112f3c6e23e9",
     },
-    "assetBundlePatterns": ["**/*"],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.sharemystack",
-      "config": {
-        "usesNonExemptEncryption": false
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.sharemystack",
+      config: {
+        usesNonExemptEncryption: false,
       },
-      "associatedDomains": [
+      associatedDomains: [
         "applinks:sharemystack.com",
         "appclips:sharemystack.com",
         "webcredentials:sharemystack.com",
-        "activitycontinuation:sharemystack.com"
+        "activitycontinuation:sharemystack.com",
       ],
-      "appStoreUrl": "https://apps.apple.com/us/app/share-my-stack/id6450111644"
+      appStoreUrl: "https://apps.apple.com/us/app/share-my-stack/id6450111644",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png",
-      "bundler": "metro"
+    web: {
+      favicon: "./assets/favicon.png",
+      bundler: "metro",
     },
-    "extra": {
-      "eas": {
-        "projectId": "987c3c40-d46a-45e7-a545-112f3c6e23e9"
-      }
+    extra: {
+      eas: {
+        projectId: "987c3c40-d46a-45e7-a545-112f3c6e23e9",
+      },
     },
-    "plugins": ["sentry-expo"],
-    "hooks": {
-      "postPublish": [
+    plugins: ["sentry-expo"],
+    hooks: {
+      postPublish: [
         {
-          "file": "sentry-expo/upload-sourcemaps",
-          "config": {
-            "organization": "feldapp",
-            "project": "sharemystack"
-          }
-        }
-      ]
-    }
-  }
-}
+          file: "sentry-expo/upload-sourcemaps",
+          config: {
+            organization: "feldapp",
+            project: "sharemystack",
+          },
+        },
+      ],
+    },
+  },
+};
