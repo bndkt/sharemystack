6342fe39a6ebb8513fd5187d1fab57247529bdc8
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-23 16:30:58 +0800

    Add seed data

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2887,10 +2887,15 @@ values
 insert into
 public.picks (stack_id, category_id, tool_id)
 values
-('fbc5602c-ebef-4282-8754-d56520486498', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '0115cd3b-869d-4058-94e1-a67863eb6045'),
-('fbc5602c-ebef-4282-8754-d56520486498', '0e93b69f-3415-4fec-a5e6-da1f779b43a6', '9ab4d55d-a1cc-402f-9fe6-d60227a6c7a6'),
-('fbc5602c-ebef-4282-8754-d56520486498', '8f5ff296-f438-40fc-8c48-b437810385af', 'f0586aef-c792-4b6c-8d5c-8c6de1b436dc'),
-('4abd2df7-bd61-457f-8297-47aa33910cc8', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '0115cd3b-869d-4058-94e1-a67863eb6045');
+('fbc5602c-ebef-4282-8754-d56520486498', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '0115cd3b-869d-4058-94e1-a67863eb6045'), -- Benedikt
+('fbc5602c-ebef-4282-8754-d56520486498', '0e93b69f-3415-4fec-a5e6-da1f779b43a6', '9ab4d55d-a1cc-402f-9fe6-d60227a6c7a6'), -- Benedikt
+('fbc5602c-ebef-4282-8754-d56520486498', '8f5ff296-f438-40fc-8c48-b437810385af', 'f0586aef-c792-4b6c-8d5c-8c6de1b436dc'), -- Benedikt
+('4abd2df7-bd61-457f-8297-47aa33910cc8', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '0115cd3b-869d-4058-94e1-a67863eb6045'), -- Nick Milo
+('7eef57cd-dfb6-481d-967e-7d3a024f19d2', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '5b6a6858-d014-45b7-8dd6-4b85372663d6'), -- Ali Abdaal, Notion
+('211b7b6b-4117-4151-b6bc-cb64214227fd', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '0e23f13f-bb06-485d-a420-2497661dc023'), -- Tiago Forte, Evernote
+('c7e04ef0-f3bd-4532-a33f-029c924c0e05', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '5b6a6858-d014-45b7-8dd6-4b85372663d6'), -- Thomas Frank, Notion
+('aeb47866-c97a-4c7a-9967-b98e0d2fea7c', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '5b6a6858-d014-45b7-8dd6-4b85372663d6'), -- Marie Poulin, Notion
+('061bce7c-3ee1-4660-ad34-4157d79a85ac', 'bf4fb3cd-38d9-4135-abc7-7f291c498366', '5b6a6858-d014-45b7-8dd6-4b85372663d6'); -- August Bradley, Notion
 
 insert into
 public.stars (user_id, stack_id, category_id, tool_id)
