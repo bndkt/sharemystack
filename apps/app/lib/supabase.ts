15029e05dceccb75e851e6170304f70fac86e924
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-26 09:07:56 +0800

    Remove supabase key

--- apps/app/lib/supabase.ts
+++ apps/app/lib/supabase.ts
@@ -12,18 +12,6 @@ const storage = {
   removeItem: (key: string) => mmkv.delete(key),
 };
 
-/*
-// TMP until Expo SDK 49
-process.env.SUPABASE_URL = "https://wmruscfwlagunazmjwhh.supabase.co";
-process.env.SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtcnVzY2Z3bGFndW5hem1qd2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYxMDU1NjgsImV4cCI6MjAwMTY4MTU2OH0.4pFZiBa1ZtkYKTtscdRM4dnBtX55iqMBur_wNZygEqQ";
-
-// TODO
-process.env.SUPABASE_URL = "http://localhost:54321";
-process.env.SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
-*/
-
 export const supabase = createClient<Database>(
   process.env.EXPO_PUBLIC_SUPABASE_URL as string,
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string,
