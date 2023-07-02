c5823ed40182fbecb35b0078a5720058a26cf79b
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-02 08:04:58 +0800

    Update metro.config.ts

--- apps/app/metro.config.ts
+++ apps/app/metro.config.ts
@@ -1,6 +1,6 @@
 // Learn more https://docs.expo.dev/guides/monorepos
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+import { getDefaultConfig } from "expo/metro-config";
+import path from "path";
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
