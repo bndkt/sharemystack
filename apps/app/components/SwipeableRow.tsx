1aaa103df619090ff2ff192892a191a6ab5c7f41
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-09 17:50:35 +0800

    Remove logs

--- apps/app/components/SwipeableRow.tsx
+++ apps/app/components/SwipeableRow.tsx
@@ -86,10 +86,10 @@ export function SwipeableRow({
           : undefined
       }
       onSwipeableOpen={(direction) => {
-        console.log(`Opening swipeable from the ${direction}`);
+        // console.log(`Opening swipeable from the ${direction}`);
       }}
       onSwipeableClose={(direction) => {
-        console.log(`Closing swipeable to the ${direction}`);
+        // console.log(`Closing swipeable to the ${direction}`);
       }}
     >
       {children}
