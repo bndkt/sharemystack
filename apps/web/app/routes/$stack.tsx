5c4c677f305df864c50a7e82010f4a8cbe7c3879
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-13 23:43:01 +0800

    Work on web

--- apps/web/app/routes/$stack.tsx
+++ apps/web/app/routes/$stack.tsx
@@ -16,20 +16,28 @@ export default function Stack() {
   const link = `https://${config.domain}/@${data.stack}`;
 
   return (
-    <div className="content-center text-center pt-32 text-white">
+    <div className="content-center text-center pt-16 text-white">
       <div className="mx-auto pb-1 w-24">
-        <a href={link}>
-          <QRCode
-            value={link}
-            bgColor="transparent"
-            fgColor="#ffffff"
-            size={256}
-            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-            viewBox={`0 0 256 256`}
-          />
-        </a>
+        <QRCode
+          value={link}
+          bgColor="transparent"
+          fgColor="#ffffff"
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          viewBox={`0 0 256 256`}
+        />
       </div>
       <h2>@{data.stack}</h2>
+      <div className="pt-16">
+        Please note that Share My Stack
+        <br /> is currently only available on iOS.
+        <br />
+        Follow us{" "}
+        <a href="https://twitter.com/sharemystack" className="font-bold">
+          @sharemystack
+        </a>{" "}
+        for updates.
+      </div>
     </div>
   );
 }
--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,3 +1,11 @@
 export default function Index() {
-  return null;
+  return (
+    <div className="pt-16 text-white text-center">
+      Follow us{" "}
+      <a href="https://twitter.com/sharemystack" className="font-bold">
+        @sharemystack
+      </a>{" "}
+      for updates.
+    </div>
+  );
 }
