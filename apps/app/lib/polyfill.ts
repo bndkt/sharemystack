a8ca9b91b056024ae48bb64ed0778ac0da9a4fb4
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-15 08:23:38 +0800

    Upgrade dependencies

--- apps/app/lib/polyfill.ts
+++ apps/app/lib/polyfill.ts
@@ -1,6 +1,6 @@
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/index.html#getting-started
 // import "react-native-get-random-values";
-import { Platform } from "react-native";
-import { setupURLPolyfill } from "react-native-url-polyfill";
-
-Platform.OS !== "web" && setupURLPolyfill();
+// import { Platform } from "react-native";
+// import { setupURLPolyfill } from "react-native-url-polyfill";
+// Platform.OS !== "web" && setupURLPolyfill();
+import "react-native-url-polyfill/auto";
