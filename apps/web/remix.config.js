1063f367bdb1bd1c12da87406a15f2c0405329f1
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-21 09:32:30 +0800

    Rename file

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
-  server: "./server.js",
+  server: "./server.ts",
   serverBuildPath: "functions/[[path]].js",
-  serverConditions: ["worker"],
+  serverConditions: ["workerd", "worker", "browser"],
   serverDependenciesToBundle: "all",
   serverMainFields: ["browser", "module", "main"],
   serverMinify: true,
@@ -15,11 +15,11 @@ module.exports = {
   // publicPath: "/build/",
   tailwind: true,
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
-    v2_headers: true,
-    v2_dev: true,
   },
 };
--- apps/web/server.ts
+++ apps/web/server.ts
@@ -1,12 +1,13 @@
+import { logDevReady } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
 
-const handleRequest = createPagesFunctionHandler({
-  build,
-  mode: process.env.NODE_ENV,
-  getLoadContext: (context) => context.env,
-});
-
-export function onRequest(context) {
-  return handleRequest(context);
+if (process.env.NODE_ENV === "development") {
+  logDevReady(build);
 }
+
+export const onRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext: (context) => ({ env: context.env }),
+  mode: process.env.NODE_ENV,
+});
