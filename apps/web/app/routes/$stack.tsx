7b34419dd9fea5dae9f1cfa6649e75da04419c6f
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-22 11:51:11 +0800

    Remove dependencies

--- apps/web/app/routes/$stack.tsx
+++ apps/web/app/routes/$stack.tsx
@@ -1,10 +1,13 @@
 import { LoaderArgs } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
-import QRCode from "react-qr-code";
 
 import { config } from "~/lib/config";
 
 export async function loader({ params }: LoaderArgs) {
+  if (params.stack?.toLowerCase().substring(0, 1) !== "@") {
+    throw new Error("Invalid stack name");
+  }
+
   return {
     stack: params.stack?.toLowerCase().substring(1),
   };
@@ -12,21 +15,13 @@ export async function loader({ params }: LoaderArgs) {
 
 export default function Stack() {
   const data = useLoaderData<typeof loader>();
+  console.log({ data });
 
   const link = `https://${config.domain}/@${data.stack}`;
 
   return (
     <div className="content-center text-center pt-16 text-white">
-      <div className="mx-auto pb-1 w-24">
-        <QRCode
-          value={link}
-          bgColor="transparent"
-          fgColor="#ffffff"
-          size={256}
-          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-          viewBox={`0 0 256 256`}
-        />
-      </div>
+      <div className="mx-auto pb-1 w-24"></div>
       <h2>@{data.stack}</h2>
       <div className="pt-16">
         Please note that Share My Stack
