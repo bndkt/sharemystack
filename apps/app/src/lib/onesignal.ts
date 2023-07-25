794e4625511f2e72f6983af23d7a9c7737a47048
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-25 12:41:53 +0800

    Downgrade OneSignal

--- apps/app/src/lib/onesignal.ts
+++ apps/app/src/lib/onesignal.ts
@@ -1,12 +1,19 @@
-import { OneSignal } from "react-native-onesignal";
+// import { OneSignal } from "react-native-onesignal"; // SDK v5
+import OneSignal from "react-native-onesignal";
 
-OneSignal.initialize(process.env.EXPO_PUBLIC_ONESIGNAL_APP_ID as string);
+OneSignal.setLocationShared(false);
 
-export function oneSignalLogin(userExternalId?: string, email?: string) {
-  userExternalId && OneSignal.login(userExternalId);
-  email && OneSignal.User.addEmail(email);
+// OneSignal.initialize(process.env.EXPO_PUBLIC_ONESIGNAL_APP_ID as string); // SDK v5
+
+OneSignal.setAppId(process.env.EXPO_PUBLIC_ONESIGNAL_APP_ID as string);
+
+export function oneSignalLogin(externalId?: string, email?: string) {
+  // externalId && OneSignal.login(externalId); // SDK v5
+  externalId && OneSignal.setExternalUserId(externalId);
+  // email && OneSignal.User.addEmail(email); // SDK v5
+  email && OneSignal.setEmail(email);
 }
 
 export function oneSignalLogout() {
-  OneSignal.logout();
+  // OneSignal.logout(); // SDK v5
 }
