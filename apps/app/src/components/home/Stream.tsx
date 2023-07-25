b8ee69514490c8c47711dfa2963897173c853a6a
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-25 21:14:31 +0800

    Add icon

--- apps/app/src/components/home/Stream.tsx
+++ apps/app/src/components/home/Stream.tsx
@@ -1,3 +1,4 @@
+import { Layers } from "@tamagui/lucide-icons";
 import { useRouter } from "expo-router";
 import { ListItem } from "tamagui";
 
@@ -22,6 +23,7 @@ export function Stream() {
             title={`${item.stackName} (@${item.stackSlug})`}
             subTitle={`added ${item.toolName} in ${item.categoryName}`}
             onPress={() => router.push(`/(tabs)/(stacks)/@${item.stackSlug}`)}
+            iconAfter={<Layers size="$1" />}
           />
         );
       }}
