778cc31bd08420171cd0a3f4b2a066bf06f8d51b
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-28 16:32:50 +0800

    Move Sentry DSN to env var

--- apps/app/src/lib/sentry.ts
+++ apps/app/src/lib/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "sentry-expo";
 
 Sentry.init({
-  dsn: "https://b8b90433e45342b08a64f3288673632b@o4504655409577984.ingest.sentry.io/4505330170200064",
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   enableInExpoDevelopment: false,
   debug: false,
 });
