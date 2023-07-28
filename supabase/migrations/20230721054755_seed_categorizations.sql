79cfbd17bc5e9beb4a0640184e9547aa94246c3b
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-07-28 21:54:35 +0800

    Add tools

--- supabase/migrations/20230721054755_seed_categorizations.sql
+++ supabase/migrations/20230721054755_seed_categorizations.sql
@@ -1,236 +1,243 @@
 -- gen_random_uuid()
 insert into public.categorizations (tool_id, category_id)
 values -- Note-taking
+    -- Obsidian
     (
         '0115cd3b-869d-4058-94e1-a67863eb6045',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Obsidian
+    -- Notion
     (
         '5b6a6858-d014-45b7-8dd6-4b85372663d6',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Notion
+    -- Apple Notes
     (
         'c60f8bf1-a3a7-4dee-a7f8-cef89a9a7f6a',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Apple Notes
+    -- Evernote
     (
         '0e23f13f-bb06-485d-a420-2497661dc023',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Evernote
+    -- RemNote
     (
         '9b4c841a-cac0-4be6-b74b-eb699a24fb46',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- RemNote
+    -- Logseq
     (
         '739e7782-5e7f-424f-9e82-0c2f39b102ce',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Logseq
+    -- Tana
     (
         'e5dcb6e8-2728-42dc-9895-544192f9cb1c',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Tana
+    -- Bear
     (
         '7d5a14f8-f5a1-4163-b9fe-075d8979f6f5',
         'bf4fb3cd-38d9-4135-abc7-7f291c498366'
     ),
-    -- Bear
     -- Password Management
+    -- 1Password
     (
         '9ab4d55d-a1cc-402f-9fe6-d60227a6c7a6',
         '0e93b69f-3415-4fec-a5e6-da1f779b43a6'
     ),
-    -- 1Password
+    -- LastPass
     (
         '776cb0c8-c0d2-4071-a9e1-e0d9eae4cbd6',
         '0e93b69f-3415-4fec-a5e6-da1f779b43a6'
     ),
-    -- LastPass
+    -- Bitwarden
     (
         'eb2a8dc6-2ad4-4228-b4da-ea41d6e95055',
         '0e93b69f-3415-4fec-a5e6-da1f779b43a6'
     ),
-    -- Bitwarden
     -- Operating System
+    -- macOS
     (
         'f0586aef-c792-4b6c-8d5c-8c6de1b436dc',
         '8f5ff296-f438-40fc-8c48-b437810385af'
     ),
-    -- macOS
+    -- Windows
     (
         'f2d26e2e-b59a-44bc-8673-340f0516457f',
         '8f5ff296-f438-40fc-8c48-b437810385af'
     ),
-    -- Windows
+    -- Linux
     (
         '92f307f2-6897-4831-9033-25833df73f6d',
         '8f5ff296-f438-40fc-8c48-b437810385af'
     ),
-    -- Linux
     -- IDE
+    -- VS Code
     (
         '68c52f82-7755-434d-bc07-1eee6bfe982f',
         'a53d4fba-35a4-49e6-baf3-0e7bf2a1188b'
     ),
-    -- VS Code
     -- () -- Vim
     -- Browser
+    -- Google Chrome
     (
         '95c4b2f8-9b08-462a-a4cd-b8e908b8b3df',
         'f899b6d5-a1e1-48fe-96c2-ec45e00425bc'
     ),
-    -- Google Chrome
+    -- Arc
     (
         'f40604f8-e501-4418-9238-880c13d914f9',
         'f899b6d5-a1e1-48fe-96c2-ec45e00425bc'
     ),
-    -- Arc
+    -- Safari
     (
         '1ce3f168-0d7a-438f-b499-515a17276230',
         'f899b6d5-a1e1-48fe-96c2-ec45e00425bc'
     ),
-    -- Safari
+    -- Firefox
     (
         '603ec4b3-4ea4-4ef7-b2a6-ec0536f58de4',
         'f899b6d5-a1e1-48fe-96c2-ec45e00425bc'
     ),
-    -- Firefox
     -- Email Service
+    -- Apple iCloud
     (
         '324d5413-cde6-4151-a71d-8dad2c7c05e0',
         '6a9095b4-132e-4625-891d-8286e92e8f92'
     ),
-    -- Apple iCloud
+    -- Google Mail
     (
         'c2ac9bb6-10ce-4b46-aefb-962d8e1de5c9',
         '6a9095b4-132e-4625-891d-8286e92e8f92'
     ),
-    -- Google Mail
+    -- Outlook.com
     (
         '877b27e1-092e-4d2e-baae-4f974a223161',
         '6a9095b4-132e-4625-891d-8286e92e8f92'
     ),
-    -- Outlook.com
+    -- Hey
+    (
+        '71291614-55cc-4b3e-b18f-35b6c0bbec2f',
+        '6a9095b4-132e-4625-891d-8286e92e8f92'
+    ),
     -- Task Management
+    -- Things
     (
         '3ce6bedb-2605-4f54-a269-89a4737b14ba',
         '0b8e259e-7683-4267-a1e7-e8cba6a39f19'
     ),
-    -- Things
+    -- Todoist
     (
         '0a2ffd37-4073-4b94-b0b7-96e03700c98f',
         '0b8e259e-7683-4267-a1e7-e8cba6a39f19'
     ),
-    -- Todoist
+    -- Microsoft To Do
     (
         'c365473b-7b08-4426-95ce-2c7a30bfa772',
         '0b8e259e-7683-4267-a1e7-e8cba6a39f19'
     ),
-    -- Microsoft To Do
+    -- Google Tasks
     (
         '3b3dff9e-eb6a-4c09-bbca-50edb27035bd',
         '0b8e259e-7683-4267-a1e7-e8cba6a39f19'
     ),
-    -- Google Tasks
     -- Mobile OS
+    -- iOS
     (
         '1a2e7f2f-192b-4c5c-88c8-4a5971caa4e9',
         '5b4d499c-114e-472f-baa9-3adc7685936c'
     ),
-    -- iOS
+    -- Android
     (
         '3c1419d1-802d-40ad-ab17-5b4240ec06b5',
         '5b4d499c-114e-472f-baa9-3adc7685936c'
     ),
-    -- Android
     -- Email Client
+    -- Microsoft Outlook
     (
         '525ad02f-f284-4f55-a2b2-d277d31bf209',
         'f370ab2a-7abb-4ad8-a501-58a25bb559f4'
     ),
-    -- Microsoft Outlook
-    -- ('', 'f370ab2a-7abb-4ad8-a501-58a25bb559f4'), -- Apple Mail
+    -- Apple Mail
+    -- ('', 'f370ab2a-7abb-4ad8-a501-58a25bb559f4'),
+    -- Superhuman
     (
         '98d9d7de-0ae1-440c-a708-2955b82bfbf1',
         'f370ab2a-7abb-4ad8-a501-58a25bb559f4'
     ),
-    -- Superhuman
+    -- Google Mail
     (
         'c2ac9bb6-10ce-4b46-aefb-962d8e1de5c9',
         'f370ab2a-7abb-4ad8-a501-58a25bb559f4'
     ),
-    -- Google Mail
     -- Calendar
+    -- Microsoft Outlook
     (
         '525ad02f-f284-4f55-a2b2-d277d31bf209',
         'd3faed2f-ea3f-4ec9-9118-e3d6db4fe42e'
     ),
-    -- Microsoft Outlook
+    -- Google Calendar
     (
         'b3c10887-f3b3-438f-9fdf-5136e86c57bd',
         'd3faed2f-ea3f-4ec9-9118-e3d6db4fe42e'
     ),
-    -- Google Calendar
+    -- Cron
     (
         'b021f050-f4af-4233-9fda-6eeb4cf65544',
         'd3faed2f-ea3f-4ec9-9118-e3d6db4fe42e'
     ),
-    -- Cron
     -- Video Conferencing
+    -- Microsoft Teams
     (
         'c481dac2-17d7-4b63-b07d-8e9fd267f499',
         '7c470aa0-e03c-4542-ba46-6983b51c8ae0'
     ),
-    -- Microsoft Teams
+    -- Google Meet
     (
         '31bd97cd-300a-494f-9712-0f50bed27e89',
         '7c470aa0-e03c-4542-ba46-6983b51c8ae0'
     ),
-    -- Google Meet
+    -- Zoom
     (
         '5f526565-81a5-4944-be2c-b6ce9cefed93',
         '7c470aa0-e03c-4542-ba46-6983b51c8ae0'
     ),
-    -- Zoom
     -- Instant Messaging
+    -- WhatsApp
     (
         '3a1fcf9e-61af-468b-b299-375f48d4ebe1',
         'ec5b8a05-80a5-4c72-abf1-0082c763d9fd'
     ),
-    -- WhatsApp
-    -- ('', 'ec5b8a05-80a5-4c72-abf1-0082c763d9fd'), -- iMessage
+    -- iMessage
+    -- ('', 'ec5b8a05-80a5-4c72-abf1-0082c763d9fd'),
+    -- Signal
     (
         '5e2a51d2-c8f2-4813-aa28-2a3be8ce6e79',
         'ec5b8a05-80a5-4c72-abf1-0082c763d9fd'
     ),
-    -- Signal
+    -- Telegram
     (
         '19dc4b7c-6f8d-4e1d-a1e4-67567bd25368',
         'ec5b8a05-80a5-4c72-abf1-0082c763d9fd'
     ),
-    -- Telegram
     -- File Hosting
+    -- OneDrive
     (
         'a37e164f-f6a9-4412-9579-083fe5f725d7',
         '86f3c1ef-3cfb-46e4-841d-3f2e1d9d1843'
     ),
-    -- OneDrive
+    -- Box
     (
         '4c55bed6-cf33-4800-b050-ee45a67bee9c',
         '86f3c1ef-3cfb-46e4-841d-3f2e1d9d1843'
     ),
-    -- Box
+    -- Dropbox
     (
         'af495155-60b7-425c-871f-28917d91c586',
         '86f3c1ef-3cfb-46e4-841d-3f2e1d9d1843'
     ),
-    -- Dropbox
     -- ('3526b556-c5da-4902-854b-267a5c71b942', ''), Salesforce
     -- ('94eba581-26a2-466b-aa06-6f84b5202cc6', ''), Discord
     -- ('98d9d7de-0ae1-440c-a708-2955b82bfbf1', ''), Superhuman
@@ -240,5 +247,20 @@ values -- Note-taking
     -- ExpressVPN
     (
         '35ecc3eb-1c52-49fd-8b8d-209c0801d9cb',
+        '05915bf6-137b-4e40-8e42-c9c453426ed2'
+    ),
+    -- ProtonVPN
+    (
+        '9d750c39-5674-43c3-8937-0e2429a53698',
+        '05915bf6-137b-4e40-8e42-c9c453426ed2'
+    ),
+    -- Surfshark
+    (
+        'd32b14bf-9a58-43f4-b9a5-1e720b05e041',
+        '05915bf6-137b-4e40-8e42-c9c453426ed2'
+    ),
+    -- NordVPN
+    (
+        '81ac7b87-a070-437b-83e5-07dabce67f44',
         '05915bf6-137b-4e40-8e42-c9c453426ed2'
     );
