932fbea63403fcf481370523bc7e3157137e1bb5
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-01 22:14:55 +0800

    Update import script

--- supabase/scripts/import.sql
+++ supabase/scripts/import.sql
@@ -1,4 +1,4 @@
 \COPY auth.users(id, aud, role) FROM './supabase/exports/users.csv' WITH CSV HEADER;
-\COPY stacks (id,created_at,updated_at,deleted_at,server_created_at,last_modified_at,name,slug,user_id,twitter,twitter_image_url,website,is_featured) FROM './supabase/exports/stacks.csv' WITH CSV HEADER;
-\COPY picks FROM './supabase/exports/picks.csv' WITH CSV HEADER;
-\COPY stars FROM './supabase/exports/stars.csv' WITH CSV HEADER;
+\COPY profiles (id,created_at,updated_at,deleted_at,server_created_at,last_modified_at,name,slug,user_id,twitter,twitter_image_url,website,description,youtube,image,is_featured) FROM './supabase/exports/stacks.csv' WITH CSV HEADER;
+-- \COPY picks FROM './supabase/exports/picks.csv' WITH CSV HEADER;
+-- \COPY stars FROM './supabase/exports/stars.csv' WITH CSV HEADER;
