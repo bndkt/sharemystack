16ffc7c3b06ea8e5719dc3eae4edcafd361956f6
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-02 18:38:07 +0800

    Add use of GitHub secrets

--- packages/seed-data-action/src/lib/supabase.ts
+++ packages/seed-data-action/src/lib/supabase.ts
@@ -4,9 +4,9 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/database.types.js";
 
 const supabaseUrl =
-  core.getInput("supabaseUrl") || (process.env.SUPABASE_URL as string);
+  core.getInput("supabaseUrl") || (process.env.GH_SUPABASE_URL as string);
 const supabaseKey =
-  core.getInput("supabaseKey") || (process.env.SUPABASE_KEY as string);
+  core.getInput("supabaseKey") || (process.env.GH_SUPABASE_KEY as string);
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: { persistSession: false },
