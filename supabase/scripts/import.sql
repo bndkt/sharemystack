02fa12dcf396dced2e22dbcfeb0317ca1b304428
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-03 15:26:34 +0800

    Update import script

--- supabase/scripts/import.sql
+++ supabase/scripts/import.sql
@@ -1,2 +1,2 @@
-\copy auth.users(id, aud, role) from './supabase/exports/users.csv' with csv header;
+--\copy auth.users(id, aud, role) from './supabase/exports/users.csv' with csv header;
 \copy profiles (id,created_at,updated_at,deleted_at,server_created_at,last_modified_at,name,slug,user_id,twitter,twitter_image_url,website,description,youtube,image,is_featured) from './supabase/exports/stacks.csv' with csv header;
