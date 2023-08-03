e90f32ff63e243debcdb0abac35304109b9f6950
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-03 23:48:28 +0800

    Add logo

--- apps/web/app/components/Header.tsx
+++ apps/web/app/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Logo } from "./Logo";
+
 export function Header() {
   return (
     <header className="text-center font-bold text-white pt-32">
-      <h1>
+      <Logo className="w-16 inline-block" />
+      <h1 className="mt-4">
         <a href="/">Share My Stack</a>
       </h1>
     </header>
