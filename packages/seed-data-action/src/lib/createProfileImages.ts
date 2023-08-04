b55d83f11738ca88ce69a77a1d7e0c51bbf8afef
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-04 21:18:28 +0800

    Add blurhash generation

--- packages/seed-data-action/src/lib/createProfileImages.ts
+++ packages/seed-data-action/src/lib/createProfileImages.ts
@@ -1,5 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
+import sharp from "sharp";
+import { encode } from "blurhash";
 
 import { supabase } from "./supabase.js";
 import { RecordIds } from "../types/types.js";
@@ -17,11 +19,11 @@ export async function createProfileImages({
   const files = await fs.readdir(imagesPath, {});
 
   for (const file of files) {
-    console.log({ file });
     if (path.extname(file) === ".webp") {
       const filePath = path.join(imagesPath, file);
 
       const imageFile = await fs.readFile(filePath, "utf-8");
+      const slug = path.basename(file, ".webp");
 
       const { data, error } = await supabase.storage
         .from("public-images")
@@ -29,6 +31,27 @@ export async function createProfileImages({
           cacheControl: "3600",
           upsert: true,
         });
+
+      const { width, height } = await sharp(filePath).metadata();
+      const buffer = await sharp(filePath)
+        .raw()
+        .ensureAlpha()
+        // .resize(metadata.width, metadata.height, { fit: "inside" })
+        .toBuffer();
+      const blurhash = encode(
+        new Uint8ClampedArray(buffer),
+        width ?? 800,
+        height ?? 600,
+        4,
+        4
+      );
+
+      if (blurhash && profileRecordIds[slug]) {
+        await supabase
+          .from("profiles")
+          .update({ blurhash })
+          .match({ id: profileRecordIds[slug] });
+      }
 
       error && console.error(error);
     }
