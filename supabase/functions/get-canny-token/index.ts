298f41ee927c9442f08377e4a8d9a28e6cce7942
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-06 12:19:10 +0800

    Upgrade dependency

--- supabase/functions/get-canny-token/index.ts
+++ supabase/functions/get-canny-token/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.197.0/http/server.ts";
 import {
   UserResponse,
   createClient,
