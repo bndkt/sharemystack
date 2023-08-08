b34d6e1c163ad32840c3812d519f80533e34a19e
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-08 15:01:36 +0800

    Update script

--- supabase/scripts/export.sql
+++ supabase/scripts/export.sql
@@ -1,2 +1,2 @@
-\copy (select stacks.* from stacks where is_featured = false) to './supabase/exports/stacks.csv' with csv header;
+\copy (select profiles.* from profiles where is_featured = false) to './supabase/exports/profiles.csv' with csv header;
 \copy (select id, aud, role from auth.users) to './supabase/exports/users.csv' with csv header;
