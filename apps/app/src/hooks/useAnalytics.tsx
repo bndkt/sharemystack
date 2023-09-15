02aabe54e71ab872f1707fb802e0c45052f184ef
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2023-09-15 11:37:19 +0800

    Update OneSignal calls

--- apps/app/src/hooks/useAnalytics.tsx
+++ apps/app/src/hooks/useAnalytics.tsx
@@ -1,24 +1,21 @@
 import { usePostHog } from "posthog-react-native";
-import OneSignal from "react-native-onesignal";
+import { OneSignal } from "react-native-onesignal";
 import * as Sentry from "sentry-expo";
 import { identifyDevice } from "vexo-analytics";
-// import { OneSignal } from "react-native-onesignal"; // SDK v5
 
 export function useAnalytics() {
   const postHog = usePostHog();
 
   function identify({ id, email }: { id?: string; email?: string }) {
-    // externalId && OneSignal.login(id); // SDK v5
-    id && OneSignal.setExternalUserId(id);
-    // email && OneSignal.User.addEmail(email); // SDK v5
-    email && OneSignal.setEmail(email);
+    id && OneSignal.login(id);
+    email && OneSignal.User.addEmail(email);
     id && identifyDevice(id);
     id && postHog?.identify(id, { email });
     Sentry.Native.setUser({ id, email });
   }
 
   function logout() {
-    // OneSignal.logout(); // SDK v5
+    OneSignal.logout();
     Sentry.Native.setUser(null);
   }
 
