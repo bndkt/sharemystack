912d81ea95c78ed7b482e6abc606572229fc9cd6
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2023-09-19 14:04:57 +0800

    Enable OneSignal

--- apps/app/src/lib/onesignal.ts
+++ apps/app/src/lib/onesignal.ts
@@ -1,10 +1,5 @@
-import { OneSignal } from "react-native-onesignal"; // SDK v5
-// import OneSignal from "react-native-onesignal";
+import { OneSignal } from "react-native-onesignal";
 
 import { config } from "./config";
 
-// Disable for SDK v5
-// OneSignal.setLocationShared(false);
-
-OneSignal.initialize(config.oneSignalAppId); // SDK v5
-// OneSignal.setAppId(config.oneSignalAppId);
+OneSignal.initialize(config.oneSignalAppId);
