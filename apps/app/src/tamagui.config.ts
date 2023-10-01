96fba845ce29c7a7568fbaaf06e9ce03a7305d39
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2023-10-01 11:28:41 +0800

    Enable fastSchemeChange

--- apps/app/src/tamagui.config.ts
+++ apps/app/src/tamagui.config.ts
@@ -7,6 +7,7 @@ const headingFont = createInterFont();
 const bodyFont = createInterFont();
 
 const appConfig = createTamagui({
+  settings: { fastSchemeChange: true },
   themes,
   defaultTheme: "dark",
   shouldAddPrefersColorThemes: false,
