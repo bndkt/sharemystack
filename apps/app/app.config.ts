950a7b1e552d734042bf560a6310fdfb2032e882
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2023-10-19 10:22:25 +0800

    Bump version

--- apps/app/app.config.ts
+++ apps/app/app.config.ts
@@ -23,7 +23,7 @@ const config: ExpoConfig = {
   slug: "sharemystack",
   scheme,
   owner: "feldapp",
-  version: "1.0.7",
+  version: "1.0.8",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
