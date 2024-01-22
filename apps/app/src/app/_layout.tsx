cbbb5db7f8ccc8484cf11bbe74c76647832e1587
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-01-22 09:44:16 +0800

    Improve Tamagui setup

--- apps/app/src/app/_layout.tsx
+++ apps/app/src/app/_layout.tsx
@@ -1,53 +1,41 @@
 import "expo-dev-client";
 import { DatabaseProvider } from "@nozbe/watermelondb/DatabaseProvider";
-import { TamaguiProvider, Theme } from "@tamagui/core";
+import { TamaguiProvider } from "@tamagui/core";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { PostHogProvider } from "posthog-react-native";
-import { useEffect, useRef, useState } from "react";
-import { AppState, useColorScheme } from "react-native";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 
 import "@/lib/vexo";
 import { config } from "@/lib/config";
 import { database } from "@/lib/watermelon";
 import { AuthProvider } from "@/providers/AuthProvider";
-import { NavigationThemeProvider } from "@/providers/NavigationThemeProvider";
 import { SyncProvider } from "@/providers/SyncProvider";
 import tamaguiConfig from "@/tamagui.config";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Layout() {
-  const appState = useRef(AppState.currentState);
-  const [appStateVisible, setAppStateVisible] = useState(appState.current);
   const colorScheme = useColorScheme();
-  const [activeColorScheme, setActiveColorScheme] = useState(colorScheme);
 
-  useEffect(() => {
-    if (appStateVisible === "active") {
-      setActiveColorScheme(colorScheme);
-    }
-  }, [appStateVisible, colorScheme]);
-
-  useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      appState.current = nextAppState;
-      setAppStateVisible(appState.current);
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-
-  const [loaded] = useFonts({
+  const [interLoaded, interError] = useFonts({
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
   });
 
-  if (!loaded) {
-    return null;
-  }
+  useEffect(() => {
+    if (interLoaded || interError) {
+      SplashScreen.hideAsync();
+    }
+  }, [interLoaded, interError]);
 
   return (
     <PostHogProvider
@@ -56,34 +44,37 @@ export default function Layout() {
         host: "https://app.posthog.com",
       }}
     >
-      <TamaguiProvider config={tamaguiConfig}>
-        <Theme name={activeColorScheme}>
-          <NavigationThemeProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              <SafeAreaProvider>
-                <DatabaseProvider database={database}>
-                  <AuthProvider>
-                    <SyncProvider>
-                      <Stack>
-                        <Stack.Screen
-                          name="(tabs)"
-                          options={{
-                            title: "Home",
-                            headerShown: false,
-                          }}
-                        />
-                        <Stack.Screen
-                          name="[profile]"
-                          options={{ headerShown: true }}
-                        />
-                      </Stack>
-                    </SyncProvider>
-                  </AuthProvider>
-                </DatabaseProvider>
-              </SafeAreaProvider>
-            </GestureHandlerRootView>
-          </NavigationThemeProvider>
-        </Theme>
+      <TamaguiProvider
+        config={tamaguiConfig}
+        defaultTheme={colorScheme === "dark" ? "dark" : "light"}
+      >
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaProvider>
+              <DatabaseProvider database={database}>
+                <AuthProvider>
+                  <SyncProvider>
+                    <Stack>
+                      <Stack.Screen
+                        name="(tabs)"
+                        options={{
+                          title: "Home",
+                          headerShown: false,
+                        }}
+                      />
+                      <Stack.Screen
+                        name="[profile]"
+                        options={{ headerShown: true }}
+                      />
+                    </Stack>
+                  </SyncProvider>
+                </AuthProvider>
+              </DatabaseProvider>
+            </SafeAreaProvider>
+          </GestureHandlerRootView>
+        </ThemeProvider>
       </TamaguiProvider>
     </PostHogProvider>
   );
